c7be873de050ddc2d007002a4b296343c2e8084f
vinhvn <vincentn337@gmail.com>
2022-01-30 15:57:56 -0500

    chore: boilerplate until craco works with cra5

---
+++ template/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import tw from "twin.macro";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p tw="bg-black">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
