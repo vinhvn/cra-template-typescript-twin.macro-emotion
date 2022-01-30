52fe4f1f675503a2c96ab1c79689dfe2556f855a
vinhvn <vincentn337@gmail.com>
2022-01-30 16:08:57 -0500

    chore: fixed typing and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import tw from "twin.macro";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import tw, { styled } from 'twin.macro';
+import logo from './logo.svg';
+import './App.css';
+
+const StyledParagraph = styled('p')`
+  ${tw`bg-black`}
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p tw="bg-black">
+        <StyledParagraph>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </StyledParagraph>
         <a
           className="App-link"
           href="https://reactjs.org"
